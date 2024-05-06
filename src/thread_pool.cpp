7fdff143c18d3789a0607ff3113c3fcabcd97492
Kaiser-Yang <624626089@qq.com>
2024-05-06 17:47:40 +0800

    Add a new feature: thread pool
    Add a thread pool and finish the unit test.
    
    See: #19.

---
+++ src/thread_pool.cpp
@@ -0,0 +1,35 @@
+#include "thread_pool.h"
+
+namespace mca{
+void ThreadPool::resize(size_t newSize) {
+    if (size() > newSize) { clear(); }
+    stopped = false;
+    while (threadQueue.size() < newSize) {
+        threadQueue.emplace([this] () {
+            std::function<void()> task;
+            while (true) {
+                {
+                    std::unique_lock<std::mutex> locker{mu};
+                    cv.wait(locker, [this] () { return stopped || !taskQueue.empty(); });
+                    if (stopped) { return; }
+                    task = taskQueue.front();
+                    taskQueue.pop();
+                }
+                task();
+            }});
+    }
+}
+
+void ThreadPool::clear() {
+    {
+        std::unique_lock<std::mutex> locker;
+        stopped = true;
+        taskQueue = std::queue<std::function<void()>>();
+        cv.notify_all();
+    }
+    while(!threadQueue.empty()) {
+        threadQueue.front().join();
+        threadQueue.pop();
+    }
+}
+}
