24edd78a07ebc1559058f1c83215e48c613eb0f9
Quentin9922 <quentincheung261@gmail.com>
2024-05-15 21:22:30 +0800

    Modify the sing thread calulation and test
    We find use `pos` and `len` can better implement multi-thread version,
    so that we updated these.
    
    see #54.

--- test/src/single_thread_matrix_calculation_test.cpp
+++ test/src/single_thread_matrix_calculation_test.cpp
@@ -30,40 +30,40 @@ protected:
 
 TEST_F(TestSinglThreadCalculation, powNumberWholeMatrix) {
     output = Matrix<>({3, 3}, 0);
-    powNumberSingleThread(a, 2, output, 0, 0, a.shape());
+    powNumberSingleThread(a, 2, output, 0, a.size());
     Matrix<> result = Matrix<>({{0, 0, 0}, {1, 1, 1}, {4, 4, 4}});
     ASSERT_TRUE(equalSingleThread(output, result, 0, output.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, powNumberSubMatrix) {
     output = Matrix<>({3, 3}, -1);
-    powNumberSingleThread(c, 2, output, 0, 1, {2, 2});
-    Matrix<> result = Matrix<>({{-1, 4, 9}, {-1, 25, 36}, {-1, -1, -1}});
+    powNumberSingleThread(c, 2, output, 1, 5);
+    Matrix<> result = Matrix<>({{-1, 4, 9}, {16, 25, 36}, {-1, -1, -1}});
     ASSERT_TRUE(equalSingleThread(output, result, 0, output.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, numberPowWholeMatrix) {
     output = Matrix<>({3, 3}, 0);
-    numberPowSingleThread(2, a, output, 0, 0, a.shape());
+    numberPowSingleThread(2, a, output, 0, a.size());
     Matrix<> result = Matrix<>({{1, 1, 1}, {2, 2, 2}, {4, 4, 4}});
     ASSERT_TRUE(equalSingleThread(output, result, 0, output.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, numberPowSubMatrix) {
     output = Matrix<>({3, 3}, -1);
-    numberPowSingleThread(2, c, output, 0, 1, {2, 2});
-    Matrix<> result({{-1, 4, 8}, {-1, 32, 64}, {-1, -1, -1}});
+    numberPowSingleThread(2, c, output, 1, 5);
+    Matrix<> result({{-1, 4, 8}, {16, 32, 64}, {-1, -1, -1}});
     ASSERT_TRUE(equalSingleThread(output, result, 0, output.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, lessWholeMatrix) {
-    ASSERT_TRUE(lessSingleThread(a, c, 0, 0, a.shape()));
-    ASSERT_FALSE(lessSingleThread(a, b, 0, 0, a.shape()));
+    ASSERT_TRUE(lessSingleThread(a, c, 0, a.size()));
+    ASSERT_FALSE(lessSingleThread(a, b, 0, a.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, lessSubMatrix) {
-    ASSERT_TRUE(lessSingleThread(a, c, 1, 0, {2, 3}));
-    ASSERT_FALSE(lessSingleThread(a, b, 1, 0, {2, 2}));
+    ASSERT_TRUE(lessSingleThread(a, c, 1, 3));
+    ASSERT_FALSE(lessSingleThread(a, b, 3, 3));
 }
 
 TEST_F(TestSinglThreadCalculation, equalWholeMatrix) {
@@ -77,27 +77,27 @@ TEST_F(TestSinglThreadCalculation, equalSubMatrix) {
 }
 
 TEST_F(TestSinglThreadCalculation, lessEqualWholeMatrix) {
-    ASSERT_TRUE(lessEqualSingleThread(a, c, 0, 0, a.shape()));
-    ASSERT_TRUE(lessEqualSingleThread(a, d, 0, 0, a.shape()));
-    ASSERT_FALSE(lessEqualSingleThread(c, b, 0, 0, a.shape()));
+    ASSERT_TRUE(lessEqualSingleThread(a, c, 0, a.size()));
+    ASSERT_TRUE(lessEqualSingleThread(a, d, 0, a.size()));
+    ASSERT_FALSE(lessEqualSingleThread(c, b, 0, c.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, lessEqualSubMatrix) {
-    ASSERT_TRUE(lessEqualSingleThread(a, c, 0, 0, {2, 3}));
-    ASSERT_TRUE(lessEqualSingleThread(a, d, 0, 0, {2, 3}));
-    ASSERT_FALSE(lessEqualSingleThread(c, a, 0, 0, {2, 3}));
+    ASSERT_TRUE(lessEqualSingleThread(a, c, 4, 4));
+    ASSERT_TRUE(lessEqualSingleThread(a, d, 4, 4));
+    ASSERT_FALSE(lessEqualSingleThread(c, a, 4, 4));
 }
 
 TEST_F(TestSinglThreadCalculation, greaterWholeMatrix) {
-    ASSERT_TRUE(greaterSingleThread(c, a, 0, 0, c.shape()));
-    ASSERT_FALSE(greaterSingleThread(d, a, 0, 0, d.shape()));
-    ASSERT_FALSE(greaterSingleThread(d, b, 0, 0, b.shape()));
+    ASSERT_TRUE(greaterSingleThread(c, a, 0, c.size()));
+    ASSERT_FALSE(greaterSingleThread(d, a, 0, d.size()));
+    ASSERT_FALSE(greaterSingleThread(d, b, 0, d.size()));
 }
 
 TEST_F(TestSinglThreadCalculation, greaterSubMatrix) {
-    ASSERT_TRUE(greaterSingleThread(c, a, 0, 1, {2, 2}));
-    ASSERT_FALSE(greaterSingleThread(a, b, 0, 1, {2, 2}));
-    ASSERT_FALSE(greaterSingleThread(a, d, 0, 1, {2, 2}));
+    ASSERT_TRUE(greaterSingleThread(c, a, 2, 3));
+    ASSERT_FALSE(greaterSingleThread(a, b, 2, 3));
+    ASSERT_FALSE(greaterSingleThread(a, d, 2, 3));
 }
 
 TEST_F(TestSinglThreadCalculation, greaterEqualWholeMatrix) {
