9aac4ff0c57330d7b823d1214fc9ae59f5fa6b14
luckygalaxy666 <393628658@qq.com>
2024-05-19 18:07:59 +0800

    Add tests for same address
    Finish some tests which check whether matrix operations we write
    can work when a and output's addresses are same.
    
    See #63.

--- test/src/same_address_test.cpp
+++ test/src/same_address_test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <cmath>
+
 #include "matrix.h"
 #include "single_thread_matrix_calculation.h"
 
@@ -7,8 +9,11 @@ namespace mca {
 namespace test {
 class TestSameAddress : public testing::Test {
 protected:
-    Matrix<> a;
-    void SetUp() override { a = Matrix<>({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}); }
+    Matrix<> a, b;
+    void SetUp() override {
+        a = Matrix<>({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        b = a;
+    }
 
     void TearDown() override {}
 };
@@ -19,18 +24,56 @@ TEST_F(TestSameAddress, powNumber) {
     ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
 }
 
-TEST_F(TestSameAddress, numberPow) {}
+TEST_F(TestSameAddress, numberPow) {
+    numberPowSingleThread(2, a, a, 0, a.size());
+    Matrix<> result = {{std::pow(2, 1), std::pow(2, 2), std::pow(2, 3)},
+                       {std::pow(2, 4), std::pow(2, 5), std::pow(2, 6)},
+                       {std::pow(2, 7), std::pow(2, 8), std::pow(2, 9)}};
+    ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
+}
 
-TEST_F(TestSameAddress, addNumber) {}
+TEST_F(TestSameAddress, addNumber) {
+    addSingleThread(2, a, a, 0, a.size());
+    Matrix<> result = {{2 + 1, 2 + 2, 2 + 3}, {2 + 4, 2 + 5, 2 + 6}, {2 + 7, 2 + 8, 2 + 9}};
+    ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
+}
 
-TEST_F(TestSameAddress, subtractNumber) {}
+TEST_F(TestSameAddress, subtractNumber) {
+    subtractSingleThread(a, 2, a, 0, a.size());
+    Matrix<> result1 = {{1 - 2, 2 - 2, 3 - 2}, {4 - 2, 5 - 2, 6 - 2}, {7 - 2, 8 - 2, 9 - 2}};
+    subtractSingleThread(2, b, b, 0, b.size());
+    Matrix<> result2 = {{2 - 1, 2 - 2, 2 - 3}, {2 - 4, 2 - 5, 2 - 6}, {2 - 7, 2 - 8, 2 - 9}};
+    ASSERT_TRUE(equalSingleThread(a, result1, 0, a.size()));
+    ASSERT_TRUE(equalSingleThread(b, result2, 0, b.size()));
+}
 
-TEST_F(TestSameAddress, multiplyNumber) {}
+TEST_F(TestSameAddress, multiplyNumber) {
+    multiplySingleThread(2, a, a, 0, a.size());
+    Matrix<> result = {{2 * 1, 2 * 2, 2 * 3}, {2 * 4, 2 * 5, 2 * 6}, {2 * 7, 2 * 8, 2 * 9}};
+    ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
+}
 
-TEST_F(TestSameAddress, divideNumber) {}
+TEST_F(TestSameAddress, divideNumber) {
+    divideSingleThread(a, 2, a, 0, a.size());
+    Matrix<> result1 = {
+        {1. / 2, 2. / 2, 3. / 2}, {4. / 2, 5. / 2, 6. / 2}, {7. / 2, 8. / 2, 9. / 2}};
+    divideSingleThread(2, b, b, 0, b.size());
+    Matrix<> result2 = {
+        {2. / 1, 2. / 2, 2. / 3}, {2. / 4, 2. / 5, 2. / 6}, {2. / 7, 2. / 8, 2. / 9}};
+    ASSERT_TRUE(equalSingleThread(a, result1, 0, a.size()));
+    ASSERT_TRUE(equalSingleThread(b, result2, 0, b.size()));
+}
 
-TEST_F(TestSameAddress, addMatrix) {}
+TEST_F(TestSameAddress, addMatrix) {
+    addSingleThread(a, a, a, 0, a.size());
+    Matrix<> result = {{1 + 1, 2 + 2, 3 + 3}, {4 + 4, 5 + 5, 6 + 6}, {7 + 7, 8 + 8, 9 + 9}};
+    ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
+}
 
-TEST_F(TestSameAddress, sustractMatrix) {}
+TEST_F(TestSameAddress, subtractMatrix) {
+    subtractSingleThread(a, a, a, 0, a.size());
+    Matrix<> result = {{1 - 1, 2 - 2, 3 - 3}, {4 - 4, 5 - 5, 6 - 6}, {7 - 7, 8 - 8, 9 - 9}};
+    ASSERT_TRUE(equalSingleThread(a, result, 0, a.size()));
+}
 }  // namespace test
 }  // namespace mca
