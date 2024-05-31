f2233500418bcbac264d9b8014e361ffadc3a1f6
Kaiser-Yang <624626089@qq.com>
2024-05-31 11:10:41 +0800

    Add a class called Diag
    Now we define a new class called `Diag`, this class receives a container
    that can be the parameter of `std::data`, e.g `std::vector`,
    `std::initializer_list`. This class is for specifying a diagonal matrix.
    For example, now if you want to create a diagonal matrix, you must use
    `Matrix<> a(Diag({1, 2, 3}))` to create a diagonal matrix whose diagonal
    elements are `1, 2, 3`.
    
    See #90.
    
    Besides, we find that assignments from a pointer, `std::vector` and
    `std::initializer_list` are not reasonable, so we remove the
    assignments. Now if you want to assign from a `std::vector`, you must
    use `a = Matrix<>(vec)`, which will make the codes more readable. The
    same applies to others (`a = Matrix<>(a.shape(), pointer, a.size())` and
    `a = Matrix<>({{1, 2, 3}, {2, 3, 4}})`).
    
    We also update `IdentityMatrix`. We find this class should only be
    constructed from the construct with no parameters. And we remove some
    redundant `explicit`, because `explicit` is for preventing implicit
    conversions, there is no need to specify `explicit` for copy
    constructors, move constructors, and constructors with no parameters.

---
+++ src/include/diag.h
@@ -0,0 +1,57 @@
+#ifndef MCA_DIAG_H
+#define MCA_DIAG_H
+#include <cstddef>
+#include <initializer_list>
+#include <vector>
+
+namespace mca {
+// non-movable, non-copyable, non-assignable
+template <class Container>
+class _Diag {
+public:
+    _Diag() = delete;
+
+    _Diag(_Diag &&other) noexcept = delete;
+    _Diag(const _Diag &other)     = delete;
+
+    _Diag &operator=(const _Diag &) = delete;
+    _Diag &operator=(_Diag &&)      = delete;
+
+    inline explicit _Diag(const Container &other);
+    inline explicit _Diag(Container &&other);
+
+    inline const typename Container::value_type &operator[](const size_t &i) const;
+    inline size_t size() const;
+
+private:
+    const Container &_diagRef;
+    Container _diag;
+};
+
+template <class Container>
+_Diag<Container>::_Diag(const Container &_diag) : _diagRef(_diag) {}
+
+template <class Container>
+inline _Diag<Container>::_Diag(Container &&_diag) : _diag(std::move(_diag)), _diagRef(_diag) {}
+
+template <class Container>
+inline const typename Container::value_type &_Diag<Container>::operator[](const size_t &i) const {
+    return std::data(_diagRef)[i];
+}
+
+template <class Container>
+inline size_t _Diag<Container>::size() const {
+    return _diagRef.size();
+}
+
+template <class Container>
+inline _Diag<std::decay_t<Container>> Diag(Container &&container) {
+    return _Diag<std::decay_t<Container>>(std::forward<Container &&>(container));
+}
+
+template <class T>
+inline _Diag<std::initializer_list<T>> Diag(std::initializer_list<T> &&container) {
+    return _Diag<std::initializer_list<T>>(std::forward<std::initializer_list<T> &&>(container));
+}
+}  // namespace mca
+#endif
