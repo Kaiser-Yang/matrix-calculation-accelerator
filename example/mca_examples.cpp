02b25ae623edc855d5048adfd985949e97d63c7e
luckygalaxy666 <393628658@qq.com>
2024-07-08 19:15:53 +0800

    Fix some mistakes of mca_examples
    Replace direct construction with copy construction of m.

--- example/mca_examples.cpp
+++ example/mca_examples.cpp
@@ -5,11 +5,11 @@
 #include <iostream>
 
 int main() {
-    using mca::Matrix, mca::Shape, std::cout;
+    using mca::Matrix, mca::Shape, std::cout, std::boolalpha;
 
     Matrix<double> a(Shape(3, 3), 1);
     Matrix<double> b(Shape(3, 3), 2);
-    double n = 2;
+    double number = 2;
 
     cout << "Matrix a:\n";
     cout << a << "\n";
@@ -17,26 +17,26 @@ int main() {
     cout << "Matrix b:\n";
     cout << b << "\n";
 
-    cout << "Number n:\n";
-    cout << n << "\n\n";
+    cout << "Number number:\n";
+    cout << number << "\n\n";
 
     cout << "a == b:\n";
-    cout << (a == b) << "\n\n";
+    cout << boolalpha << (a == b) << "\n\n";
 
     cout << "a != b:\n";
-    cout << (a != b) << "\n\n";
+    cout << boolalpha << (a != b) << "\n\n";
 
     cout << "a < b:\n";
-    cout << (a < b) << "\n\n";
+    cout << boolalpha << (a < b) << "\n\n";
 
     cout << "a <= b:\n";
-    cout << (a <= b) << "\n\n";
+    cout << boolalpha << (a <= b) << "\n\n";
 
     cout << "a > b:\n";
-    cout << (a > b) << "\n\n";
-
+    cout << boolalpha << (a > b) << "\n\n";
+  
     cout << "a >= b:\n";
-    cout << (a >= b) << "\n\n";
+    cout << boolalpha << (a >= b) << "\n\n";
 
     cout << "a + b:\n";
     cout << a + b << "\n";
@@ -47,131 +47,132 @@ int main() {
     cout << "a * b:\n";
     cout << a * b << "\n";
 
-    cout << "a + n:\n";
-    cout << a + n << "\n";
+    cout << "a + number:\n";
+    cout << a + number << "\n";
 
-    cout << "n + a:\n";
-    cout << n + a << "\n";
+    cout << "number + a:\n";
+    cout << number + a << "\n";
 
-    cout << "a - n:\n";
-    cout << a - n << "\n";
+    cout << "a - number:\n";
+    cout << a - number << "\n";
 
-    cout << "n - a:\n";
-    cout << n - a << "\n";
+    cout << "number - a:\n";
+    cout << number - a << "\n";
 
-    cout << "a * n:\n";
-    cout << a * n << "\n";
+    cout << "a * number:\n";
+    cout << a * number << "\n";
 
-    cout << "n * a:\n";
-    cout << n * a << "\n";
+    cout << "number * a:\n";
+    cout << number * a << "\n";
 
-    cout << n / a << "\n";
+    cout << "a / number:\n";
+    cout << a / number << "\n";
+
+    cout << "number / a:\n";
+    cout << number / a << "\n";
 
     Matrix<double> m(Shape(3, 3), 1);
-    cout << "Matrix m ~ m11:\n";
+    cout << "Matrix m :\n";
     cout << m << "\n";
 
-    Matrix<double> m1(Shape(3, 3), 1);
+    Matrix<double> m1 = m;
     cout << "m1 += m:\n";
     m1 += m;
     cout << m1 << "\n";
 
-    Matrix<double> m2(Shape(3, 3), 1);
+    Matrix<double> m2 = m;
     cout << "m2 -= m:\n";
     m2 -= m;
     cout << m2 << "\n";
 
-    Matrix<double> m3(Shape(3, 3), 1);
+    Matrix<double> m3 = m;
     cout << "m3 *= m:\n";
     m3 *= m;
     cout << m3 << "\n";
 
-    Matrix<double> m4(Shape(3, 3), 1);
-    cout << "m4 += n:\n";
-    m4 += n;
+    Matrix<double> m4 = m;
+    cout << "m4 += number:\n";
+    m4 += number;
     cout << m4 << "\n";
 
-    Matrix<double> m5(Shape(3, 3), 1);
-    cout << "n += m5:\n";
-    n += m5;
+    Matrix<double> m5 = m;
+    cout << "number += m5:\n";
+    number += m5;
     cout << m5 << "\n";
 
-    Matrix<double> m6(Shape(3, 3), 1);
-    cout << "m6 -= n:\n";
-    m6 -= n;
+    Matrix<double> m6 = m;
+    cout << "m6 -= number:\n";
+    m6 -= number;
     cout << m6 << "\n";
 
-    Matrix<double> m7(Shape(3, 3), 1);
-    cout << "n -= m7:\n";
-    n -= m7;
+    Matrix<double> m7 = m;
+    cout << "number -= m7:\n";
+    number -= m7;
     cout << m7 << "\n";
 
-    Matrix<double> m8(Shape(3, 3), 1);
-    cout << "m8 *= n:\n";
-    m8 *= n;
+    Matrix<double> m8 = m;
+    cout << "m8 *= number:\n";
+    m8 *= number;
     cout << m8 << "\n";
 
-    Matrix<double> m9(Shape(3, 3), 1);
-    cout << "n *= m9:\n";
-    n *= m9;
+    Matrix<double> m9 = m;
+    cout << "number *= m9:\n";
+    number *= m9;
     cout << m9 << "\n";
 
-    Matrix<double> m10(Shape(3, 3), 1);
-    cout << "m10 /= n:\n";
-    m10 /= n;
+    Matrix<double> m10 = m;
+    cout << "m10 /= number:\n";
+    m10 /= number;
     cout << m10 << "\n";
 
-    Matrix<double> m11(Shape(3, 3), 1);
-    cout << "n /= m11:\n";
-    n /= m11;
+    Matrix<double> m11 = m;
+    cout << "number /= m11:\n";
+    number /= m11;
     cout << m11 << "\n";
 
-    cout << "Matrix m12 ~ m19 \n";
-    cout << Matrix<int> ({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}) << "\n";
+    cout << "Matrix M \n";
+    Matrix<int> m0({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    cout << m0 << "\n";
 
-    Matrix<int> m12({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m12 = m0;
     cout << "Transpose a matrix:\n";
     transpose(m12);
     cout << m12 << "\n";
     
-    Matrix<int> m13({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    Matrix<int> m13T(m13.shape(), 0);
-    cout << "Transpose a matrix but store the result into another matrix:\n";
-    transpose(m13, m13T);
-    cout << m13T << "\n";
+    Matrix<int> output = m0;
+    cout << "Transpose a matrix but store the result into matrix output:\n";
+    transpose(m0, output);
+    cout << output << "\n";
 
-    Matrix<int> m14({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m14 = m0;
     cout << "Raise a matrix to the power of n:\n";
-    pow(m14, 2);
+    pow(m14, number);
     cout << m14 << "\n";
 
-    Matrix<int> m15({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    Matrix<int> m15P(m15.shape(), 0);
+    output = m0;
     cout << "Raise a matrix to the power of n but store the result into another matrix:\n";
-    pow(m15, 2, m15P);
-    cout << m15P << "\n";
+    pow(m0, number, output);
+    cout << output << "\n";
 
-    Matrix<int> m16({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    output = m0;
     cout << "Calculate the numberPow of a matrix, the number set to be n:\n";
-    numberPow(n, m16);
-    cout << m16 << "\n";
+    numberPow(number, output);
+    cout << output << "\n";
 
-    Matrix<int> m17({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    Matrix<int> m17P(m17.shape(), 0);
-    cout << "Calculate the numberPow of a matrix, the number set to be n, but store the result into another matrix:\n";
-    numberPow(n, m17, m17P);
-    cout << m17P << "\n";
+    output = m0;
+    cout << "Calculate the numberPow of a matrix, the number set to be number, but store the result into another matrix:\n";
+    numberPow(number, m0, output);
+    cout << output << "\n";
 
-    Matrix<int> m18({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    output = m0;
     cout << "Calculate the powNumber of a matrix, the number set to be n:\n";
-    powNumber(m18, n);
-    cout << m18 << "\n";
+    powNumber(output, number);
+    cout << output << "\n";
 
-    Matrix<int> m19({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    Matrix<int> m19P(m19.shape(), 0);
-    cout << "Calculate the powNumber of a matrix, the number set to be n, but store the result into another matrix:\n";
-    powNumber(m19, n, m19P);
-    cout << m19P << "\n";
+    output = m0;
+    cout << "Calculate the powNumber of a matrix, the number set to be number, but store the result into another matrix:\n";
+    powNumber(m0, number, output);
+    cout << output << "\n";
     
     return 0;
 }
