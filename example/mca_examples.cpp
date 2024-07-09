9e28774398ef911b9fb0ec8a32ba50ccdf4bb564
luckygalaxy666 <393628658@qq.com>
2024-07-09 14:45:41 +0800

    Fix some mistakes
    Update m and M with tmp and tmp1 and delete an empty line.

--- example/mca_examples.cpp
+++ example/mca_examples.cpp
@@ -1,4 +1,3 @@
-
 #include <example_utility.h>  // for operator<<
 #include <mca/matrix.h>
 #include <mca/mca.h>
@@ -71,107 +70,107 @@ int main() {
     cout << "number / a:\n";
     cout << number / a << "\n";
 
-    Matrix<double> m(Shape(3, 3), 1);
-    cout << "Matrix m :\n";
-    cout << m << "\n";
+    Matrix<double> tmp(Shape(3, 3), 1);
+    cout << "Matrix tmp :\n";
+    cout << tmp << "\n";
 
-    Matrix<double> m1 = m;
-    cout << "m1 += m:\n";
-    m1 += m;
+    Matrix<double> m1 = tmp;
+    cout << "m1 += tmp:\n";
+    m1 += tmp;
     cout << m1 << "\n";
 
-    Matrix<double> m2 = m;
-    cout << "m2 -= m:\n";
-    m2 -= m;
+    Matrix<double> m2 = tmp;
+    cout << "m2 -= tmp:\n";
+    m2 -= tmp;
     cout << m2 << "\n";
 
-    Matrix<double> m3 = m;
-    cout << "m3 *= m:\n";
-    m3 *= m;
+    Matrix<double> m3 = tmp;
+    cout << "m3 *= tmp:\n";
+    m3 *= tmp;
     cout << m3 << "\n";
 
-    Matrix<double> m4 = m;
+    Matrix<double> m4 = tmp;
     cout << "m4 += number:\n";
     m4 += number;
     cout << m4 << "\n";
 
-    Matrix<double> m5 = m;
+    Matrix<double> m5 = tmp;
     cout << "number += m5:\n";
     number += m5;
     cout << m5 << "\n";
 
-    Matrix<double> m6 = m;
+    Matrix<double> m6 = tmp;
     cout << "m6 -= number:\n";
     m6 -= number;
     cout << m6 << "\n";
 
-    Matrix<double> m7 = m;
+    Matrix<double> m7 = tmp;
     cout << "number -= m7:\n";
     number -= m7;
     cout << m7 << "\n";
 
-    Matrix<double> m8 = m;
+    Matrix<double> m8 = tmp;
     cout << "m8 *= number:\n";
     m8 *= number;
     cout << m8 << "\n";
 
-    Matrix<double> m9 = m;
+    Matrix<double> m9 = tmp;
     cout << "number *= m9:\n";
     number *= m9;
     cout << m9 << "\n";
 
-    Matrix<double> m10 = m;
+    Matrix<double> m10 = tmp;
     cout << "m10 /= number:\n";
     m10 /= number;
     cout << m10 << "\n";
 
-    Matrix<double> m11 = m;
+    Matrix<double> m11 = tmp;
     cout << "number /= m11:\n";
     number /= m11;
     cout << m11 << "\n";
 
-    cout << "Matrix M \n";
-    Matrix<int> m0({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    cout << m0 << "\n";
+    cout << "Matrix tmp1 \n";
+    Matrix<int> tmp1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    cout << tmp1 << "\n";
 
-    Matrix<int> m12 = m0;
+    Matrix<int> m12 = tmp1;
     cout << "Transpose a matrix:\n";
     transpose(m12);
     cout << m12 << "\n";
     
-    Matrix<int> output = m0;
+    Matrix<int> output = tmp1;
     cout << "Transpose a matrix but store the result into matrix output:\n";
-    transpose(m0, output);
+    transpose(tmp1, output);
     cout << output << "\n";
 
-    Matrix<int> m14 = m0;
+    Matrix<int> m14 = tmp1;
     cout << "Raise a matrix to the power of n:\n";
     pow(m14, number);
     cout << m14 << "\n";
 
-    output = m0;
+    output = tmp1;
     cout << "Raise a matrix to the power of n but store the result into another matrix:\n";
-    pow(m0, number, output);
+    pow(tmp1, number, output);
     cout << output << "\n";
 
-    output = m0;
+    output = tmp1;
     cout << "Calculate the numberPow of a matrix, the number set to be n:\n";
     numberPow(number, output);
     cout << output << "\n";
 
-    output = m0;
+    output = tmp1;
     cout << "Calculate the numberPow of a matrix, the number set to be number, but store the result into another matrix:\n";
-    numberPow(number, m0, output);
+    numberPow(number, tmp1, output);
     cout << output << "\n";
 
-    output = m0;
+    output = tmp1;
     cout << "Calculate the powNumber of a matrix, the number set to be n:\n";
     powNumber(output, number);
     cout << output << "\n";
 
-    output = m0;
+    output = tmp1;
     cout << "Calculate the powNumber of a matrix, the number set to be number, but store the result into another matrix:\n";
-    powNumber(m0, number, output);
+    powNumber(tmp1, number, output);
     cout << output << "\n";
     
     return 0;
